7edf41862a2f955a19ae931892534056af1abf0f
user <gayan@github.com>
2023-02-22 12:03:37 +0400

    deletion of redundancy files

--- Spelling-Battle/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Spelling_Battle.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
